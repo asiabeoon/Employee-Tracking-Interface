e50d4935e1cc38ac09eda50c48b2247e1e5a814b
asiabeoon <a.hashbey@gmail.com>
2023-06-17 15:17:08 -0400

    created databases and put in some starter code

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 4444;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'LevelLove02!20!!',
+    database: ''
+  },
+
+  // {
+  //   host: 'localhost', // 127.0.0.1 is another name for localhost
+  //   // host: '127.0.0.1',
+  //   user: 'root',
+  //   password: '',
+  //   database: 'classlist_db' // should exist
+  // },
+  console.log(`Connected to the database.`)
+);
+
+// Query database
+db.query('SELECT * FROM students', function (err, results) {
+  console.log(results);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- table-async-await/db/schema.sql
+++ table-async-await/db/schema.sql
@@ -1,19 +1,10 @@
-DROP DATABASE IF EXISTS registrar_db;
-CREATE DATABASE registrar_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE registrar_db;
+USE employee_db;
 
-
-CREATE TABLE instructors (
-  id INT AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  active BOOLEAN NOT NULL,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  PRIMARY KEY(id)
-);
-
-CREATE TABLE courses (
+-- Table for Company departments
+CREATE TABLE departments (
   id INT AUTO_INCREMENT NOT NULL,
   course_title VARCHAR(30) NOT NULL,
   course_description TEXT NOT NULL,
@@ -25,6 +16,32 @@ CREATE TABLE courses (
   ON DELETE SET NULL,
   PRIMARY KEY(id)
 );
+
+-- Table for Company Roles may not need
+CREATE TABLE company roles (
+  id INT AUTO_INCREMENT NOT NULL,
+  course_title VARCHAR(30) NOT NULL,
+  course_description TEXT NOT NULL,
+  active BOOLEAN NOT NULL,
+  date_added DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  instructor_id INT,
+  FOREIGN KEY (instructor_id)
+  REFERENCES instructors(id)
+  ON DELETE SET NULL,
+  PRIMARY KEY(id)
+);
+
+-- Table of employees
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  active BOOLEAN NOT NULL,
+  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY(id)
+);
+
+
 
 CREATE TABLE students (
   id INT AUTO_INCREMENT NOT NULL,
