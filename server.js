a48230bae476554579548f2d4fc12982ec21e28e
asiabeoon <a.hashbey@gmail.com>
2023-06-19 14:45:41 -0400

    added working function to add Employee, Add Role and updateEmployee data functions are giving errors

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const inquirer = require('inquirer');
 // Import and require mysql2
 const mysql = require('mysql2');
-const { table } = require('table');
-
+const {table}  = require('table');
 
 
 // Connect to database
@@ -16,36 +15,12 @@ const db = mysql.createConnection(
     database: 'employee_db'
   },
 
-  // {
-  //   host: 'localhost', // 127.0.0.1 is another name for localhost
-  //   // host: '127.0.0.1',
-  //   user: 'root',
-  //   password: '',
-  //   database: 'classlist_db' // should exist
-  // },
+
   console.log(`Connected to the database.`)
 );
 
-// Opening Header from https://www.npmjs.com/package/table
+// Add Opening Header from https://www.npmjs.com/package/table
 
-// const config = {
-// columnDefault: {
-//   width: 10,
-// },
-// header: {
-//   alignment: 'center',
-//   content: 'EMPLOYEE MANAGER',
-// },
-// }
-
-// console.log(table(config));
-
-// // Query database
-// db.query('SELECT * FROM students', function (err, results) {
-//   console.log(results);
-// });
-
-// Created an array of questions for user input using example from ReadMe Generator
 
 // Start Inquirer Prompts
 const userPrompts = () => {
@@ -55,13 +30,16 @@ const userPrompts = () => {
           type: 'list',
           message: 'What would you like to do? (Use arrow Keys)',
           choices: [
+            'View All Departments',
+            'Add Department',
+            'View All Roles',
+            'Add Role',
             'View All Employees',
             'Add Employee',
             'Update Employee Role',
-            'View All Roles',
-            'Add Role',
-            'View All Departments',
-            'Add Department',
+            'Remove Department',
+            'Remove Role',
+            'Remove Employee',
             'Quit'
             ]
         }
@@ -69,6 +47,22 @@ const userPrompts = () => {
     
     .then((answers) => {
       const {choices} = answers;
+
+        if (choices === 'View All Departments') {
+        viewAllDepartments();
+         }
+
+        if (choices === 'Add Department') {
+        addDepartment();
+        }
+
+        if (choices === 'View All Roles') {
+            viewAllRoles();
+          }
+    
+        if (choices === 'Add Role') {
+            addRole();
+          }
   
         if (choices === 'View All Employees') {
             viewAllEmployees();
@@ -82,29 +76,29 @@ const userPrompts = () => {
           updateEmployeeRole();
         }
   
-        if (choices === 'View All Roles') {
-          viewAllRoles();
+        if (choices === 'Remove Department') {
+          removeDepartment();
         }
   
-        if (choices === 'Add Role') {
-          addRole();
-        }
-  
-        if (choices === 'View All Departments') {
-          viewAllDepartments();
-        }
-  
-        if (choices === 'Add Department') {
-          addDepartment();
-        }
-  
+        if (choices === 'Remove Role') {
+            removeRole();
+          }
+
+        if (choices === 'Remove Employee') {
+            removeEmployee();
+          }
+    
         if (choices === 'Quit') {
             connection.end();
         }
       
     })
   };
+  
+userPrompts()
 
+
+//   Functions to view departments
   function viewAllDepartments(){
     db.query('SELECT * FROM departments', function (err, results) {
         console.table(results);
@@ -125,42 +119,165 @@ const userPrompts = () => {
         userPrompts()
          });
   }
-  
+
+
+  //  Functions to Update employee Data --This works---
+const updateEmployeeRole = () => {
+    inquirer.prompt([
+      {
+        name: 'employeeName',
+        type: 'input',
+        message: 'Enter the name of the employee whose role you want to update:',
+      },
+      {
+        name: 'newRole',
+        type: 'input',
+        message: 'Enter the name of the new role:',
+      },
+    ])
+    .then((answers) => {
+      const { employeeName, newRole } = answers;
+      const sql = `
+        SELECT id
+        FROM role
+        WHERE title = ?;
+      `;
+// Following Code created with the assistance of CHAT GPT by 
+      connection.query(sql, newRole, (error, results) => {
+        if (error) throw error;
+        const roleId = results[0].id;
+        const updateSql = `
+          UPDATE employee
+          SET role_id = ?
+          WHERE CONCAT(first_name, ' ', last_name) = ?;
+        `;
+        connection.query(updateSql, [roleId, employeeName], (error, results) => {
+          if (error) throw error;
+          console.log(`${results.affectedRows} employee updated`);
+          userPrompts();
+        });
+      });
+    });
+  };
+
+ // Function to add a role
+const addRole = () => {
+    // Query the departments table for department names to use in role creation prompt
+    const query = `SELECT department_name FROM departments`;
+    db.query(query, (err, results) => {
+      if (err) throw err;
+      // Transform the results into an array of department names for use in the Inquirer prompt
+      const departmentChoices = results.map(({  department_name }) => ({
+        name: department_name
+      }));
+      // Inquirer prompts for role information
+      inquirer.prompt([
+        {
+          name: 'title',
+          type: 'input',
+          message: 'Enter the title of the role:'
+        },
+        {
+          name: 'salary',
+          type: 'number',
+          message: 'Enter the salary for this role:'
+        },
+        {
+          name: 'department',
+          type: 'list',
+          message: 'Choose the department for this role:',
+          choices: departmentChoices
+        }
+      ])
+      .then((answers) => {
+        // Insert the new role into the roles table
+        const query = `INSERT INTO role SET ?`;
+        const { title, salary, department } = answers;
+        const newRole = { title, salary, department_id: department };
+        db.query(query, newRole, (err, result) => {
+          if (err) throw err;
+          console.log(`\n${result.affectedRows} New role added successfully!\n`);
+          // Call the user prompts function again
+          userPrompts();
+        });
+      });
+    });
+  };
+       
+// Function to Add employee
+const addEmployee = () => {
+    // Get role information to generate choices for inquirer prompt
+    let rolesList = [];
+    db.query(`SELECT * FROM role`, (err, rows) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // Push each role title and id to rolesList array
+      rows.forEach((role) => {
+        rolesList.push({
+          name: role.title,
+          value: role.id
+        });
+      });
+      // Get manager information to generate choices for inquirer prompt
+      let managersList = [];
+      db.query(`SELECT * FROM employee`, (err, rows) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        // Push each employee name and id to managersList array
+        rows.forEach((employee) => {
+          managersList.push({
+            name: `${employee.first_name} ${employee.last_name}`,
+            value: employee.id
+          });
+        });
+        managersList.push({
+          name: "None",
+          value: null
+        });
+        // Prompt user for new employee information
+        inquirer.prompt([
+          {
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?"
+          },
+          {
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?"
+          },
+          {
+            name: "role",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: rolesList
+          },
+          {
+            name: "manager",
+            type: "list",
+            message: "Who is the employee's manager?",
+            choices: managersList
+          },
+        ]).then((answers) => {
+          // Add new employee to database
+          db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+            [answers.firstName, answers.lastName, answers.role, answers.manager],
+            (err, rows) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              console.log(`Added ${answers.firstName} ${answers.lastName} to the database!`);
+              userPrompts();
+            });
+        });
+      });
+    });
+  };
 
 
 
-userPrompts()
-
-
-// 
-// const sql = `INSERT INTO movies (movie_name)
-//     VALUES (?)`;
-// // Change [ into Inquirer prompt]
-//   const params = [body.movie_name];
-  
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: body
-//     });
-//   });
-  
-// const sql = `INSERT INTO movies (movie_name)
-//     VALUES (?)`;
-// // Change [ into Inquirer prompt]
-//   const params = [body.movie_name];
-  
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: body
-//     });
-//   });
